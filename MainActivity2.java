c646f2918486cc187c41b0e144be18b6f83b988c
Sunidhi Chattar <CHATTARSUNIDHI@GMAIL.COM>
2021-09-04 16:45:07 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,111 @@
+package com.verzeo.medicinemajorproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+
+public class MainActivity2 extends AppCompatActivity {
+    public FloatingActionButton fab;
+
+    ImageView me,hoss,calender;
+    TextView d1,t1;
+    EditText nam, dose;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main2);
+
+        t1 = findViewById(R.id.time);
+        d1 = findViewById(R.id.date);
+        nam = findViewById(R.id.name);
+        dose = findViewById(R.id.dose);
+
+
+
+        me = findViewById(R.id.user);
+        me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity5();
+            }
+        });
+
+        calender = findViewById(R.id.calender);
+        calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity6();
+            }
+        });
+
+        hoss = findViewById(R.id.hosp);
+        hoss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity7();
+            }
+        });
+
+        fab = findViewById(R.id.addreminde);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){ openMainActivity3();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+
+    private void openMainActivity7() {
+        Intent intent = new Intent(this, MainActivity7.class);
+        startActivity(intent);
+    }
+
+    private void openMainActivity6() {
+        Intent intent = new Intent(this, MainActivity6.class);
+        startActivity(intent);
+
+    }
+
+    private void openMainActivity5() {
+        Intent intent = new Intent(this, MainActivity5.class);
+        startActivity(intent);
+    }
+
+    public void openMainActivity3( ) {
+                Intent intent = new Intent(this,MainActivity3.class);
+                startActivity(intent);
+
+
+    }
+
+
+}
